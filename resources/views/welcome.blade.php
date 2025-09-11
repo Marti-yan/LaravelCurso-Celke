0eeee0c77deb2ff80ba1612349ea5ed4016feaf0
Marti-yan <yanloufraga2@gmail.com>
2025-09-10 23:02:57 -0300

    Inicio do curso de laravel | Criar o projeto

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>MartinsCoders</title>
+
+
+</head>
+
+<body>
+    <h1>Bem vindo ao curso de Laravel 12</h1>
+    <h2>Professor: Celke</h2>
+    <h3>Aluno: MartinsCoders</h3>
+</body>
+
+</html>
